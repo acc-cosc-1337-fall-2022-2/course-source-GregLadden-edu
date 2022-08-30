7965726ebd9a4e76966ec5152d7c262c70363168
Greg Ladden <gregory.ladden@gmail.com>
2022-08-29 19:28:09 -0500

    test push

---
+++ cmake-build-debug/test/homework/02_expressions/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/02_expressions
+# Build directory: /Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/cmake-build-debug/test/homework/02_expressions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hw_02_tests "/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/cmake-build-debug/test/homework/02_expressions/hw_02_tests" "-s")
+set_tests_properties(hw_02_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/02_expressions/CMakeLists.txt;3;add_test;/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/02_expressions/CMakeLists.txt;0;")
---
+++ cmake-build-debug/test/homework/03_decisions/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/03_decisions
+# Build directory: /Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/cmake-build-debug/test/homework/03_decisions
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hw_03_tests "/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/cmake-build-debug/test/homework/03_decisions/hw_03_tests" "-s")
+set_tests_properties(hw_03_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/03_decisions/CMakeLists.txt;3;add_test;/Users/gregladden/Documents/Programming Fund II/course-source-GregLadden-edu/test/homework/03_decisions/CMakeLists.txt;0;")
