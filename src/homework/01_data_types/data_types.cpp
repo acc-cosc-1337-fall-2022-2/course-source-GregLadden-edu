45d8bb22ca2b601a525ae941b4aa117d8912074f
gregladden <greg.ladden@g.austincc.edu>
2022-09-18 00:13:06 -0500

    updates

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -3,11 +3,12 @@
 //example
 int add_numbers(int numOne, int numTwo)
 {
-	return numOne + numTwo;
+    return numOne + numTwo;
 }
 
 //write function code here
-
-
-
-
+int multiply_numbers(int numOne)
+{
+    auto numTwo = 5;
+    return numOne * numTwo;
+}
