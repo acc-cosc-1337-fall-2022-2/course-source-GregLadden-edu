b0b9bdf55ff4ea69043cee5d70b3f83a548e991d
gregladden <greg.ladden@g.austincc.edu>
2022-09-18 00:32:47 -0500

    cleaning up repo

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,18 @@
 #include "expressions.h"
 
 //example
-int add_numbers(int num1, int num2)
+int add_numbers(int numOne, int numTwo)
 {
-	return num1 + num2;
+    return numOne + numTwo;
 }
 
 //write function code here
+auto get_sales_tax_amount(double meal_amount)
+{
+    return meal_amount * .0675;
+}
 
-
-
-
+auto get_tip_amount(double meal_amount, double tip_rate)
+{
+    return meal_amount * tip_rate;
+}
