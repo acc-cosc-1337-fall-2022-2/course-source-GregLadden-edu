18ae725554bf8b1011aa27ec978b7fd12ac77dfd
gregladden <greg.ladden@g.austincc.edu>
2022-09-27 23:36:44 -0500

    removing old function in header file

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,6 +1,5 @@
 //add include statements
 #include<iostream>
-#include<string>
 
 using std::string;
 /*
@@ -9,4 +8,3 @@ Write prototype for function(s)
 int factoral(int num);
 int gcd(int num1, int num2);
 int run_menu();
-string user_confirm_exit();
