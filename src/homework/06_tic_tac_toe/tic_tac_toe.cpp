7ae99f3e419566267cbd96e85453731dc52a8288
gregladden <greg.ladden@g.austincc.edu>
2022-11-05 01:55:23 -0500

    updated tic-tac-toe game to check for a winner and print out a winner or cats game.

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -6,9 +6,125 @@
 using std::count;
 using std::cout;
 
+bool TicTacToe::check_row_win() {
+    string x = "X";
+    string o = "O";
+
+    if(pegs[0] == x && pegs[1] == x && pegs[2] == x) {
+        return true;
+    }
+    if(pegs[0] == o && pegs[1] == o && pegs[2] == o) {
+        return true;
+    }
+    if(pegs[3] == x && pegs[4] == x && pegs[5] == x) {
+        return true;
+    }
+    if(pegs[3] == o && pegs[4] == o && pegs[5] == o) {
+        return true;
+    }
+    if(pegs[6] == x && pegs[7] == x && pegs[8] == x) {
+        return true;
+    }
+    if(pegs[6] == o && pegs[7] == o && pegs[8] == o) {
+        return true;
+    }
+
+    return false;
+}
+
+bool TicTacToe::check_column_win() {
+    string x = "X";
+    string o = "O";
+
+    if(pegs[0] == x && pegs[3] == x && pegs[6] == x) {
+        return true;
+    }
+    if(pegs[0] == o && pegs[3] == o && pegs[6] == o) {
+        return true;
+    }
+    if(pegs[1] == x && pegs[4] == x && pegs[7] == x) {
+        return true;
+    }
+    if(pegs[1] == o && pegs[4] == o && pegs[7] == o) {
+        return true;
+    }
+    if(pegs[2] == x && pegs[5] == x && pegs[8] == x) {
+        return true;
+    }
+    if(pegs[2] == o && pegs[5] == o && pegs[8] == o) {
+        return true;
+    }
+
+    return false;
+}
+
+bool TicTacToe::check_diagonal_win() {
+    string x = "X";
+    string o = "O";
+
+    if(pegs[0] == x && pegs[4] == x && pegs[8] == x) {
+        return true;
+    }
+    if(pegs[0] == o && pegs[4] == o && pegs[8] == o) {
+        return true;
+    }
+    if(pegs[6] == x && pegs[4] == x && pegs[2] == x) {
+        return true;
+    }
+    if(pegs[6] == o && pegs[4] == o && pegs[2] == o) {
+        return true;
+    }
+    if(pegs[2] == x && pegs[5] == x && pegs[8] == x) {
+        return true;
+    }
+    if(pegs[2] == o && pegs[5] == o && pegs[8] == o) {
+        return true;
+    }
+
+    return false;
+}
+
+void TicTacToe::set_winner() {
+    if(player == "X"){
+        winner = "0";
+    } else {
+        winner = "X";
+    }
+}
+
+string TicTacToe::get_winner(std::string winner) {
+    return winner;
+}
+
 bool TicTacToe::game_over()
 {
-    return check_board_full();
+    if(check_row_win()){
+        set_winner();
+        winner = get_winner(winner);
+        cout<<winner<<" is the winner\n";
+        return true;
+    }
+    if(check_column_win()){
+        set_winner();
+        winner = get_winner(winner);
+        cout<<winner<<" is the winner\n";
+        return true;
+    }
+    if(check_diagonal_win()){
+        set_winner();
+        winner = get_winner(winner);
+        cout<<winner<<" is the winner\n";
+        return true;
+    }
+
+    if (check_board_full()){
+//        set_winner();
+        winner = "C";
+        cout<<winner<<" - Looks like we have a cats game.\n";
+        return true;
+    }
+        return check_board_full();
+
 }
 
 void TicTacToe::start_game(string first_player)
